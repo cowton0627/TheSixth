09090354663133f63d7991b9ba3a69aeab8b4b29
鄭淳澧 <cowton0517@gmail.com>
2021-11-30 10:36:07 +0800

    Initial Commit

---
+++ TheSixth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSixth
+//
+//  Created by Chun-Li Cheng on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
